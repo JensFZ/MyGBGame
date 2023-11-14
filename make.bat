daf424a6eea0b12ef43149840eabd7eaeb54a80f
Jens Fischer <JensFZ@gmail.com>
2023-11-14 23:25:52 +0100

    pathes fixed

--- make.bat
+++ make.bat
@@ -1,3 +1,3 @@
 cls
-C:\Users\JensF\Desktop\Development\GB\gbdk\bin\lcc.exe -Wa-l -Wl-m -Wl-j -c -o main.o main.c
-C:\Users\JensF\Desktop\Development\GB\gbdk\bin\lcc.exe -Wa-l -Wl-m -Wl-j -o main.gb main.o
+..\gbdk\bin\lcc.exe -Wa-l -Wl-m -Wl-j -c -o main.o main.c
+..\gbdk\bin\lcc.exe -Wa-l -Wl-m -Wl-j -o main.gb main.o
