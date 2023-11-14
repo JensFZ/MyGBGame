eb4bcea5882b4c184aefaf369dbc851b67cb2f67
Jens Fischer <JensFZ@gmail.com>
2023-11-14 23:19:06 +0100

    cls added at beginning

--- make.bat
+++ make.bat
@@ -1,2 +1,3 @@
+cls
 C:\Users\JensF\Desktop\Development\GB\gbdk\bin\lcc.exe -Wa-l -Wl-m -Wl-j -c -o main.o main.c
 C:\Users\JensF\Desktop\Development\GB\gbdk\bin\lcc.exe -Wa-l -Wl-m -Wl-j -o main.gb main.o
